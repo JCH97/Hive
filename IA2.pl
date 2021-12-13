22c683f52e79582ea008c3085bcc50fad3281ba9
Osmany <osmanypr2013@gmail.com>
2021-12-13 17:59:00 -0500

    tiny

--- IA2.pl
+++ IA2.pl
@@ -26,13 +26,13 @@
 
 % minimax_depth(3).
 
-board(0, 0, m, w, 1, 0).
-board(-1, -1, b, w, 2, 0).
-board(-2, 0, s, b, 3, 0).
-board(2, 0, a, w, 4, 0).
-board(-1, 1, g, w, 5, 0).
-board(1, 1, l, w, 6, 0).
-board(0, 2, q, b, 7, 0).
+% board(0, 0, m, w, 1, 0).
+% board(-1, -1, b, w, 2, 0).
+% board(-2, 0, s, b, 3, 0).
+% board(2, 0, a, w, 4, 0).
+% board(-1, 1, g, w, 5, 0).
+% board(1, 1, l, w, 6, 0).
+% board(0, 2, q, b, 7, 0).
 
 % board(1, 1, b, w, 6, 0).
 % board(-2, 0, b, w, 3, 0).
